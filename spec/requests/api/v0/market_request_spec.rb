e38b50fff17f8fe73ac653d26a73074c380a69f7
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-05 21:40:09 -0700

    Tests:API request passing

--- spec/requests/api/v0/market_request_spec.rb
+++ spec/requests/api/v0/market_request_spec.rb
@@ -5,11 +5,41 @@ describe "Market Money API" do
     create_list(:market, 3)
 
     get '/api/v0/markets'
-    markets = JSON.parse(response.body)
+    markets = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(response).to be_successful
-    require 'pry'; binding.pry
     expect(markets.count).to eq(3)
+    markets.each do |market|
 
+      expect(market).to have_key(:id)
+      expect(market[:id]).to be_an(String)
+
+      expect(market[:attributes]).to have_key(:name)
+      expect(market[:attributes][:name]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:street)
+      expect(market[:attributes][:street]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:city)
+      expect(market[:attributes][:city]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:county)
+      expect(market[:attributes][:county]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:state)
+      expect(market[:attributes][:state]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:zip)
+      expect(market[:attributes][:zip]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:lat)
+      expect(market[:attributes][:lat]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:lon)
+      expect(market[:attributes][:lon]).to be_a(String)
+
+      expect(market[:attributes]).to have_key(:vendor_count)
+      expect(market[:attributes][:vendor_count]).to be_a(Integer)
+    end
   end
 end

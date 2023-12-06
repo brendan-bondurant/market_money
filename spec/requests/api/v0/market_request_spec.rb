36a93cae7f7d2f4dfa083069005b6595b7a7c730
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 09:11:46 -0700

    Test:Market request show

--- spec/requests/api/v0/market_request_spec.rb
+++ spec/requests/api/v0/market_request_spec.rb
@@ -42,4 +42,40 @@ describe "Market Money API" do
       expect(market[:attributes][:vendor_count]).to be_a(Integer)
     end
   end
+  it 'shows a single market' do
+    id = create(:market).id
+
+    get "/api/v0/markets/#{id}"
+    market = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(market).to have_key(:id)
+      expect(market[:id]).to be_an(String)
+
+      expect(market).to have_key(:name)
+      expect(market[:name]).to be_a(String)
+
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:zip)
+      expect(market[:zip]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+
+      expect(market).to have_key(:vendor_count)
+      expect(market[:vendor_count]).to be_a(Integer)
+    
+  end
 end

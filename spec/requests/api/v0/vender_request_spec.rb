afa1e3dd6a0aae844db095718f1f34ea9151db51
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 13:17:07 -0700

    Test:Tests for attributes

--- spec/requests/api/v0/vender_request_spec.rb
+++ spec/requests/api/v0/vender_request_spec.rb
@@ -3,9 +3,29 @@ require 'rails_helper'
 RSpec.describe "Market Money API" do
   describe 'single vendors' do
     it 'can show one vendor' do
-      id = create(:vendor).id
+      vendor1 = create(:vendor)
+      id = vendor1.id
       get "/api/v0/vendors/#{id}"
       expect(response).to be_successful
+
+      vendor = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(vendor).to have_key(:id)
+      expect(vendor[:id]).to be_an(String)
+
+      expect(vendor[:attributes]).to have_key(:name)
+      expect(vendor[:attributes][:name]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:description)
+      expect(vendor[:attributes][:description]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:contact_name)
+      expect(vendor[:attributes][:contact_name]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:contact_phone)
+      expect(vendor[:attributes][:contact_phone]).to be_a(String)
+
+      expect(vendor[:attributes]).to have_key(:credit_accepted)
+      expect(vendor[:attributes][:credit_accepted]).to be(true).or be(false)
     end
   end
 end

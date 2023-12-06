9313a0fd47c8ffc4f355c811e8e12683ea5579c9
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 09:21:30 -0700

    Test:Error tests added

--- spec/requests/api/v0/market_request_spec.rb
+++ spec/requests/api/v0/market_request_spec.rb
@@ -48,34 +48,47 @@ describe "Market Money API" do
     get "/api/v0/markets/#{id}"
     market = JSON.parse(response.body, symbolize_names: true)[:data]
     expect(market).to have_key(:id)
-      expect(market[:id]).to be_an(String)
+    expect(market[:id]).to be_an(String)
 
-      expect(market).to have_key(:name)
-      expect(market[:name]).to be_a(String)
+    expect(market).to have_key(:name)
+    expect(market[:name]).to be_a(String)
 
-      expect(market).to have_key(:street)
-      expect(market[:street]).to be_a(String)
+    expect(market).to have_key(:street)
+    expect(market[:street]).to be_a(String)
 
-      expect(market).to have_key(:city)
-      expect(market[:city]).to be_a(String)
+    expect(market).to have_key(:city)
+    expect(market[:city]).to be_a(String)
 
-      expect(market).to have_key(:county)
-      expect(market[:county]).to be_a(String)
+    expect(market).to have_key(:county)
+    expect(market[:county]).to be_a(String)
 
-      expect(market).to have_key(:state)
-      expect(market[:state]).to be_a(String)
+    expect(market).to have_key(:state)
+    expect(market[:state]).to be_a(String)
 
-      expect(market).to have_key(:zip)
-      expect(market[:zip]).to be_a(String)
+    expect(market).to have_key(:zip)
+    expect(market[:zip]).to be_a(String)
 
-      expect(market).to have_key(:lat)
-      expect(market[:lat]).to be_a(String)
+    expect(market).to have_key(:lat)
+    expect(market[:lat]).to be_a(String)
 
-      expect(market).to have_key(:lon)
-      expect(market[:lon]).to be_a(String)
+    expect(market).to have_key(:lon)
+    expect(market[:lon]).to be_a(String)
 
-      expect(market).to have_key(:vendor_count)
-      expect(market[:vendor_count]).to be_a(Integer)
+    expect(market).to have_key(:vendor_count)
+    expect(market[:vendor_count]).to be_a(Integer)
     
+  end
+  it 'shows an error if invalid id' do
+
+    get "/api/v0/markets/123412433124"
+    # market = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Couldn't find Market with 'id'=123412433124")
   end
 end

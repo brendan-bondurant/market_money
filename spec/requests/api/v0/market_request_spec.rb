75fd500d580a5467bb29265261cd4d4ac0cbd211
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 11:29:26 -0700

    Test:Test for vendor attributes under market passes

--- spec/requests/api/v0/market_request_spec.rb
+++ spec/requests/api/v0/market_request_spec.rb
@@ -102,24 +102,25 @@ describe "Market Money API" do
 
     expect(response).to be_successful
     expect(new_market.vendors.count).to eq(2)
+    vendors = JSON.parse(response.body, symbolize_names: true)[:data]
+    vendors.each do |vendor|
+      expect(vendor).to have_key(:id)
+      expect(vendor[:id]).to be_an(String)
 
-    # new_market.vendors. each do |vendor|
-    #   expect(vendor).to have_key(:id)
-    #   expect(vendor[:id]).to be_an(String)
-    #   expect(vendor[:attributes]).to have_key(:name)
-    #   expect(vendor[:attributes][:name]).to be_a(String)
+      expect(vendor[:attributes]).to have_key(:name)
+      expect(vendor[:attributes][:name]).to be_a(String)
 
-    #   expect(vendor[:attributes]).to have_key(:description)
-    #   expect(vendor[:attributes][:description]).to be_a(String)
+      expect(vendor[:attributes]).to have_key(:description)
+      expect(vendor[:attributes][:description]).to be_a(String)
 
-    #   expect(vendor[:attributes]).to have_key(:contact_name)
-    #   expect(vendor[:attributes][:contact_name]).to be_a(String)
+      expect(vendor[:attributes]).to have_key(:contact_name)
+      expect(vendor[:attributes][:contact_name]).to be_a(String)
 
-    #   expect(vendor[:attributes]).to have_key(:contact_phone)
-    #   expect(vendor[:attributes][:contact_phone]).to be_a(String)
+      expect(vendor[:attributes]).to have_key(:contact_phone)
+      expect(vendor[:attributes][:contact_phone]).to be_a(String)
 
-    #   expect(vendor[:attributes]).to have_key(:credit_accepted)
-    #   expect(vendor[:attributes][:credit_accepted]).to be_a(Boolean)
-    # end
+      expect(vendor[:attributes]).to have_key(:credit_accepted)
+      # expect(vendor[:attributes][:credit_accepted]).to be_a(Boolean) How to test?
+    end
   end
 end

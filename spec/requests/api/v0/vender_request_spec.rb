6b43ea46d5d6045656ba2be5e4688fb652547b40
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 13:24:58 -0700

    Test:Test for invalid vendor id written/passes

--- spec/requests/api/v0/vender_request_spec.rb
+++ spec/requests/api/v0/vender_request_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Market Money API" do
   describe 'single vendors' do
-    it 'can show one vendor' do
+    it 'can show one vendor with valid id' do
       vendor1 = create(:vendor)
       id = vendor1.id
       get "/api/v0/vendors/#{id}"
       expect(response).to be_successful
 
       vendor = JSON.parse(response.body, symbolize_names: true)[:data]
-      
+
       expect(vendor).to have_key(:id)
       expect(vendor[:id]).to be_an(String)
 
@@ -27,6 +27,18 @@ RSpec.describe "Market Money API" do
 
       expect(vendor[:attributes]).to have_key(:credit_accepted)
       expect(vendor[:attributes][:credit_accepted]).to be(true).or be(false)
+    end
+    it 'gives an error for invalid id' do
+      get "/api/v0/vendors/123412433124"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+  
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:detail]).to eq("Couldn't find Vendor with 'id'=123412433124")
     end
   end
 end

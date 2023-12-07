fc0d001e9d9d0811f6e8a148cac80b238aeefa57
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-07 14:51:30 -0700

    Revist:Sad Path 2 not functioning

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -2,6 +2,13 @@ class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
 
-  validates_presence_of :market_id
-  validates_presence_of :vendor_id
+  validate :market_or_vendor_must_be_unique
+
+  private
+
+  def market_or_vendor_must_be_unique
+    if MarketVendor.where(market_id: market_id).exists? && MarketVendor.where(vendor_id: vendor_id).exists?
+      errors.add(:base, "Validation failed: Market vendor association between market and vendor already exists")
+    end
+  end
 end
--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -11,14 +11,38 @@ RSpec.describe "Market Vendor" do
     expect(new_association[:attributes][:market_id]).to eq(market.id)
     expect(new_association[:attributes][:vendor_id]).to eq(vendor.id)
   end
-  it 'will fail if there is an invalid id' do
-
-    post '/api/v0/market_vendors', params: { market_id: "123456789", vendor_id: "123456789" }, headers: headers
+  it 'will fail if there is an invalid market id' do
+    vendor = create(:vendor)
+    post '/api/v0/market_vendors', params: { market_id: "123456789", vendor_id: vendor.id }, headers: headers
     data = JSON.parse(response.body, symbolize_names: true)
     # require 'pry'; binding.pry
     expect(data[:errors]).to be_a(Array)
     expect(data[:errors].first[:status]).to eq("404")
     expect(data[:errors].first[:detail]).to eq("Validation failed: Market must exist")
   
+  end
+  it 'will fail if there is an invalid vendor id' do
+    market = create(:market)
+    post '/api/v0/market_vendors', params: { market_id: market.id, vendor_id: "123456789" }, headers: headers
+    data = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Vendor must exist")
+  
+  end
+  xit 'will tell you if a conflicting market vendor already exists' do
+    market = create(:market)
+    vendor = create(:vendor)
+    market_vendor = MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
+    # post '/api/v0/market_vendors', params: { market_id: market_vendor.market_id, vendor_id: market_vendor.vendor_id }, headers: headers
+
+    post '/api/v0/market_vendors', params: { market_vendor: { market_id: market_vendor.market_id, vendor_id: market_vendor.vendor_id } }, headers: headers
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Market vendor association between market and vendor already exists")
+    expect(data[:errors].first[:status]).to eq("422")
   end
 end

ad8eaedce6d126b34abc6bd7033ebc4c0424dbaf
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-07 13:32:20 -0700

    Feat:Sad path 1 tested

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,12 +1,55 @@
 class Api::V0::MarketVendorsController < ApplicationController
   
   def create
+    market_id = params[:market_id]
+    vendor_id = params[:vendor_id]
+    market = Market.find(market_id)
+    vendor = Vendor.find(vendor_id)
     new_mv = MarketVendor.new(market_vendor_params)
+    new_mv.save
     render json: MarketVendorSerializer.new(new_mv), status: :created
+    # else
+    #   render json: ErrorSerializer.new(ErrorMessage.new(new_mv.errors.full_messages.join(", "), 422))
+    #     .serialize_json, status: :unprocessable_entity
+    rescue ActiveRecord::RecordNotFound => exception
+      not_valid_response(exception)
+    end
   end
 
   private
   def market_vendor_params
     params.require(:market_vendor).permit(:market_id, :vendor_id)
   end
-end
+  
+  def not_valid_response(exception)
+    if exception.message.include?("Market")
+      render json: {
+            "errors": [
+                {
+                    "status": "404",
+                    "detail": "Validation failed: Market must exist"
+                }
+            ]
+        }
+    elsif exception.message.include?("Vendor")
+      render json: {
+        "errors": [
+            {
+                "status": "404",
+                "detail": "Validation failed: Vendor must exist"
+            }
+        ]
+    }
+    # elsif
+    #   render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+    #     .serialize_json, status: :not_found
+    end
+  end
+
+#   {
+#     "errors": [
+#         {
+#             "detail": "Validation failed: Market must exist"
+#         }
+#     ]
+# }
--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe "Market Vendor" do
     expect(new_association[:attributes][:market_id]).to eq(market.id)
     expect(new_association[:attributes][:vendor_id]).to eq(vendor.id)
   end
+  it 'will fail if there is an invalid id' do
+
+    post '/api/v0/market_vendors', params: { market_id: "123456789", vendor_id: "123456789" }, headers: headers
+    data = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Market must exist")
+  
+  end
 end

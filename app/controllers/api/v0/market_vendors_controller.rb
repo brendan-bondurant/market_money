a4f8888d8de43e60958b9ef355c37b2030893cf1
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 21:45:15 -0700

    Feat:Happy path marketvendor update test

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,6 +1,12 @@
 class Api::V0::MarketVendorsController < ApplicationController
   
   def create
-    
+    new_mv = MarketVendor.new(market_vendor_params)
+    render json: MarketVendorSerializer.new(new_mv), status: :created
+  end
+
+  private
+  def market_vendor_params
+    params.require(:market_vendor).permit(:market_id, :vendor_id)
   end
 end
--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Market Vendor" do
   it 'should create a new association' do
-    post '/api/v0/market_vendors'
-    
-  
+    headers = {"CONTENT_TYPE" => "application/json"}  
+    market = create(:market)
+    vendor = create(:vendor)
+    post '/api/v0/market_vendors', params: { market_id: market.id, vendor_id: vendor.id }.to_json, headers: headers
+    new_association = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(response).to have_http_status(201)
+    expect(new_association[:attributes][:market_id]).to eq(market.id)
+    expect(new_association[:attributes][:vendor_id]).to eq(vendor.id)
   end
 end

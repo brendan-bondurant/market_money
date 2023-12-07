f2f659670644210a73f15bb63368f689235449b2
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-06 20:43:26 -0700

    Debug:Error on show all vendors fixed

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -3,7 +3,7 @@ class Api::V0::VendorsController < ApplicationController
 
   def index
     market = Market.find(params[:market_id])
-    render json: VendorSerializer.new(Vendor.all)
+    render json: VendorSerializer.new(market.vendors.all)
   end
 
   def show
